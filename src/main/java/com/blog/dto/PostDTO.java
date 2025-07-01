3814b3d5cd6b52a9d2c427c90296060583753805
gajendra-ingle <gajendraingle01@gmail.com>
2025-07-01 12:27:51 +0530

    update filed in post

--- src/main/java/com/blog/dto/PostDTO.java
+++ src/main/java/com/blog/dto/PostDTO.java
@@ -7,8 +7,6 @@ import java.util.List;
 import com.blog.entities.Comment;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
