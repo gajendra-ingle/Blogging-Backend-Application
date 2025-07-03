2a97ddc43a387c0d0e66e84ae38dcd5c17f2ae51
gajendra-ingle <gajendraingle01@gmail.com>
2025-07-04 00:35:52 +0530

    comment service logic impl

--- src/main/java/com/blog/services/Impl/CommentServiceImpl.java
+++ src/main/java/com/blog/services/Impl/CommentServiceImpl.java
@@ -1,10 +1,15 @@
 package com.blog.services.Impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.blog.dto.CommentDTO;
+import com.blog.entities.Comment;
+import com.blog.entities.Post;
+import com.blog.expections.ResourceNotFoundException;
 import com.blog.repositories.CommentRepositories;
+import com.blog.repositories.PostRepositories;
 import com.blog.services.CommentService;
 
 @Service
@@ -13,14 +18,28 @@ public class CommentServiceImpl implements CommentService {
 	@Autowired
 	private CommentRepositories commentRepositories;
 
+	@Autowired
+	private PostRepositories postRepositories;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@Override
-	public CommentDTO createCommect(CommentDTO commentdto, Integer postId) {
-		return null;
+	public CommentDTO createCommect(CommentDTO commentDto, Integer postId) {
+		Post post = postRepositories.findById(postId)
+				.orElseThrow(() -> new ResourceNotFoundException("Post", "post id ", postId));
+
+		Comment comment = modelMapper.map(commentDto, Comment.class);
+		comment.setPost(post);
+		Comment savedComment = commentRepositories.save(comment);
+		return modelMapper.map(savedComment, CommentDTO.class);
 	}
 
 	@Override
 	public void deletComment(Integer commentId) {
-
+		Comment comment = commentRepositories.findById(commentId)
+				.orElseThrow(() -> new ResourceNotFoundException("Comment", "CommentId", commentId));
+		commentRepositories.delete(comment);
 	}
 
 }
